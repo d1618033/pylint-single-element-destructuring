1d79427331bca5fe9f6676e94a0f1b15371a76c7
David Sternlicht <d1618033@gmail.com>
2020-11-21 10:01:42 +0200

    Fix integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,43 +1,43 @@
 import os
+import subprocess
 from collections import namedtuple
 
-import pylint
 import pytest
 
 PylintError = namedtuple("PylintError", "file, row, col, err, message")
 
 
-@pytest.fixture
-def run_pylint(capsys):
-    def _parse_output(output):
-        start_parsing = False
-        errors = []
-        for line in output.splitlines():
-            if "Module errors" in line:
-                start_parsing = True
-            if start_parsing:
-                split_line = line.split(":")
-                if len(split_line) != 5:
-                    continue
-                file, row, col, err, message = list(map(str.strip, split_line))
-                row = int(row)
-                col = int(col)
-                errors.append(PylintError(file, row, col, err, message))
-        return errors
+def _parse_output(output):
+    start_parsing = False
+    errors = []
+    lines = output.splitlines()
+    for line in lines:
+        split_line = line.split(":")
+        if len(split_line) != 5:
+            continue
+        file, row, col, err, message = list(map(str.strip, split_line))
+        row = int(row)
+        col = int(col)
+        errors.append(PylintError(file, row, col, err, message))
+    return errors
 
-    def _run(args):
-        pylint.PylintRun(args, do_exit=False)
-        output = capsys.readouterr().out
-        return _parse_output(output)
 
-    return _run
+def run_pylint(file_name):
+    output = (
+        subprocess.Popen(
+            ["pylint", file_name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
+        .stdout.read()
+        .decode()
+    )
+    return _parse_output(output)
 
 
 EXAMPLES_FOLDER = os.path.join(os.path.dirname(__file__), "..", "examples")
 
 
-def test_examples_error(run_pylint):
-    actual = run_pylint([os.path.join(EXAMPLES_FOLDER, "errors.py")])
+def test_examples_error():
+    actual = run_pylint(os.path.join(EXAMPLES_FOLDER, "errors.py"))
     message = "Uses single element destructuring (single-element-destructuring)"
     file = "examples/errors.py"
     error = "W0001"
@@ -54,6 +54,6 @@ def test_examples_error(run_pylint):
     assert actual == expected
 
 
-def test_examples_no_error(run_pylint):
-    actual = run_pylint([os.path.join(EXAMPLES_FOLDER, "no_errors.py")])
+def test_examples_no_error():
+    actual = run_pylint(os.path.join(EXAMPLES_FOLDER, "no_errors.py"))
     assert len(actual) == 0
